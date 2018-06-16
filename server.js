8ffddd6607468f6a2c14d382cb106dd86634b3c2
Benz Inthra <benzinthra@gmail.com>
2018-06-16 18:25:28 +1000

    5

--- server.js
+++ server.js
@@ -6,5 +6,19 @@ const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
-
 server.listen(port);
+
+// 404 error request
+router.render = (req, res) => {
+	if (res.statusCode == 404){
+		res.status(404).jsonp({
+			errorCode: 404,
+ 			description: "Not found"
+  		});
+	} else  {
+		res.jsonp({
+			statusCode: res.statusCode,
+    		body: res.locals.data
+  		});
+	}
+};
